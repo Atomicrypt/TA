4c125c385eb80c524b93ef83524bfc91b66f2085
Tyler (Edmund) Felicidario <felicidario.ty@gmail.com>
2023-03-28 09:45:58 -0700

    function prototype implementation

--- cs433_assign_starter-main/assign3/scheduler_priority_rr.cpp
+++ cs433_assign_starter-main/assign3/scheduler_priority_rr.cpp
@@ -12,3 +12,27 @@
 
 // TODO: add implementation of SchedulerFCFS constructor, destrcutor and 
 // member functions init, print_results, and simulate here
+SchedulerPriorityRR::SchedulerPriorityRR(int time_quantum) {
+    processTotal = 0;
+    this->time_quantum = time_quantum;
+    avgWait = 0.0;
+    avgTurnaround = 0.0;
+}
+
+SchedulerPriorityRR::~SchedulerPriorityRR() {
+
+}
+
+void SchedulerPriorityRR::init(std::vector<PCB>& process_list) {
+    processTotal = process_list.size();
+
+}
+
+void SchedulerPriorityRR::print_results() {
+    std::cout << "Average turn-around time = " << avgTurnaround 
+              << ", Average waiting = " << avgWait << "\n";
+}
+
+void SchedulerPriorityRR::simulate() {
+
+}
