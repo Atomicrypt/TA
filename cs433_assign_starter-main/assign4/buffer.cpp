de617b2a94fb477f8f181ad89ffb444571e00630
ccchameleon <dybrrt150@gmail.com>
2023-04-15 16:18:37 -0700

    implemented print function & removed TODOs

--- cs433_assign_starter-main/assign4/buffer.cpp
+++ cs433_assign_starter-main/assign4/buffer.cpp
@@ -14,43 +14,77 @@
 #include <iostream>
 #include "buffer.h"
 
-// TODO: Add your implementation of the buffer class here
+/**
+ * @brief Construct a new Buffer object
+ * @param size the size of the buffer
+ */
 Buffer::Buffer(int size) {
     count = 0;
     maxSize = size;
 }
 
+/**
+ * @brief Destroy the Buffer object
+ */
 Buffer::~Buffer() {
     
 }
 
+/**
+ * @brief Insert an item into the buffer
+ * @param item the item to insert
+ * @return true if successful
+ * @return false if not successful
+ */
 bool Buffer::insert_item(buffer_item item) {
     if (!is_full()) {
-        items.push(item);
+        items.push_back(item);
         count++;
         return true;
     }
     return false;
 }
 
+
+/**
+ * @brief Remove an item from the buffer
+ * @param item the item to remove
+ * @return true if successful
+ * @return false if not successful
+ */
 bool Buffer::remove_item(buffer_item *item) {
     if (!is_empty()) {
         *item = items.front();
-        items.pop();
+        items.pop_front();
         count--;
         return true;
     }
     return false;
 }
 
+
+/**
+ * @brief Get the size of the buffer
+ * @return the size of the buffer
+ */
 int Buffer::get_size() {
     return maxSize;
 }
 
+
+/**
+ * @brief Get the number of items in the buffer
+ * @return the number of items in the buffer
+ */
 int Buffer::get_count() {
     return count;
 }
 
+
+/**
+ * @brief Chceck if the buffer is empty
+ * @return true if the buffer is empty, else false
+ */
 bool Buffer::is_empty() {
     if (count == 0)
         return true;
@@ -58,6 +92,11 @@ bool Buffer::is_empty() {
         return false;
 }
 
+
+/**
+ * @brief Check if the buffer is full
+ * @return true if the buffer is full, else false
+ */
 bool Buffer::is_full() {
     if (count >= maxSize)
         return true;
@@ -65,18 +104,18 @@ bool Buffer::is_full() {
         return false;
 }
 
+/**
+ * @brief Print the buffer
+ */
 void Buffer::print_buffer() {
-    std::cout << "Buffer: [";
+    cout << "Buffer: [";
 
-    /*if (!is_empty()) {
-        // Iterate through the list and display
-        for (std::queue<buffer_item>::iterator it = items.front(); it != items.back(); ++it) {
-            std::cout << *it;
-            if (it != --items.back()) {
-                std::cout << ", ";
-            }
+    for (size_t i = 0; i < items.size(); i++) {
+        cout << items[i];
+        if (i != items.size() - 1) {
+            cout << ", ";
         }
-    }*/
+    }
 
-    std::cout << "]" << std::endl;
+    cout << "]"<<endl;
 }
