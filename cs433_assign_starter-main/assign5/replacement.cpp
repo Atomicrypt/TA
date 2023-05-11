95e75d45b5032c60885d774687645cf8868b2af4
Tyler (Edmund) Felicidario <felicidario.ty@gmail.com>
2023-05-11 11:08:27 -0700

    removed return statement in access_page()

--- cs433_assign_starter-main/assign5/replacement.cpp
+++ cs433_assign_starter-main/assign5/replacement.cpp
@@ -40,7 +40,6 @@ bool Replacement::access_page(int page_num, bool is_write)
     if (page_table[page_num].valid == true) {
         touch_page(page_num);
         
-        // return true;
     }
     // If the page is not valid but free frames are available, it calls the load_page function.
     if (page_table[page_num].valid == false && frameCount < totalFrames) {
